613c210c52e65dbaea20fcfb40dcf682bdcecf49
reeja1046 <reejagrace1046@gmail.com>
2023-12-07 16:18:21 +0530

    After fetching current location nd date

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sky_cast_weatherapp/controller/global_controller.dart';
+import 'package:sky_cast_weatherapp/widgets/header.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,6 +16,24 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Obx(
+          (() => globalController.checkLoading().isTrue
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView(
+                  scrollDirection: Axis.vertical,
+                  children: const [
+                     SizedBox(
+                      height: 20,
+                    ),
+                    HeaderWidget()
+                  ],
+                )),
+        ),
+      ),
+    );
   }
 }
