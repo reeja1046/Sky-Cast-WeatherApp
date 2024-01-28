19734d6cb694d95a0bbf617c5c029aced5d996a8
reeja1046 <reejagrace1046@gmail.com>
2024-01-28 19:26:53 +0530

    Final Commit

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -1,76 +1,76 @@
-import 'package:geolocator/geolocator.dart';
-import 'package:get/get.dart';
-import 'package:sky_cast_weatherapp/api/fetch_weather.dart';
-import 'package:sky_cast_weatherapp/model/weather_data.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:get/get.dart';
+// import 'package:sky_cast_weatherapp/api/fetch_weather.dart';
+// import 'package:sky_cast_weatherapp/model/weather_data.dart';
 
-class GlobalController extends GetxController {
-  //create variables
-  final RxBool _isLoading = true.obs;
-  final RxDouble _latitude = 0.0.obs;
-  final RxDouble _longitude = 0.0.obs;
-  final RxInt _currentIndex = 0.obs;
+// class GlobalController extends GetxController {
+//   //create variables
+//   final RxBool _isLoading = true.obs;
+//   final RxDouble _latitude = 0.0.obs;
+//   final RxDouble _longitude = 0.0.obs;
+//   final RxInt _currentIndex = 0.obs;
 
-  final weatherData = WeatherData().obs;
+//   final weatherData = WeatherData().obs;
 
-  //get variables  by creating instance
-  RxBool checkLoading() => _isLoading;
-  RxDouble getLatitude() => _latitude;
-  RxDouble getLongitude() => _longitude;
-  WeatherData getWeatherData(){
-    return weatherData.value;
-  }
+//   //get variables  by creating instance
+//   RxBool checkLoading() => _isLoading;
+//   RxDouble getLatitude() => _latitude;
+//   RxDouble getLongitude() => _longitude;
+//   WeatherData getWeatherData(){
+//     return weatherData.value;
+//   }
 
-  @override
-  void onInit() {
-    if (_isLoading.isTrue) {
-      getLocation();
-    } else{
-      getIndex();
-    }
-    super.onInit();
-  }
+//   @override
+//   void onInit() {
+//     if (_isLoading.isTrue) {
+//       getLocation();
+//     } else{
+//       getIndex();
+//     }
+//     super.onInit();
+//   }
 
-  getLocation() async {
-    bool isServiceEnabled;
-    LocationPermission locationPermission;
+//   getLocation() async {
+//     bool isServiceEnabled;
+//     LocationPermission locationPermission;
 
-    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+//     isServiceEnabled = await Geolocator.isLocationServiceEnabled();
 
-    //return if service is not enabled
-    if (!isServiceEnabled) {
-      return Future.error('Location is not enabled');
-    }
+//     //return if service is not enabled
+//     if (!isServiceEnabled) {
+//       return Future.error('Location is not enabled');
+//     }
 
-    //status of permission
-    locationPermission = await Geolocator.checkPermission();
+//     //status of permission
+//     locationPermission = await Geolocator.checkPermission();
 
-    if (locationPermission == LocationPermission.deniedForever) {
-      return Future.error('Location Permission is denied forever');
-    } else if (locationPermission == LocationPermission.denied) {
-      //request permission
-      locationPermission = await Geolocator.requestPermission();
-      if (locationPermission == LocationPermission.denied) {
-        return Future.error('Location Permission is denied');
-      }
-    }
+//     if (locationPermission == LocationPermission.deniedForever) {
+//       return Future.error('Location Permission is denied forever');
+//     } else if (locationPermission == LocationPermission.denied) {
+//       //request permission
+//       locationPermission = await Geolocator.requestPermission();
+//       if (locationPermission == LocationPermission.denied) {
+//         return Future.error('Location Permission is denied');
+//       }
+//     }
 
-    //update the current location by giving the latitude and longitude
-    return await Geolocator.getCurrentPosition(
-            desiredAccuracy: LocationAccuracy.high)
-        .then((value) {
-      //update our latitude and longitude
-      _latitude.value = value.latitude;
-      _longitude.value = value.longitude;
-      //calling our weather api
-      return FetchWeatherApi().processData(value.latitude, value.longitude).then((value) {
-        weatherData.value = value;
-         _isLoading.value = false;
-      }
-     );
-    });
-  }
+//     //update the current location by giving the latitude and longitude
+//     return await Geolocator.getCurrentPosition(
+//             desiredAccuracy: LocationAccuracy.high)
+//         .then((value) {
+//       //update our latitude and longitude
+//       _latitude.value = value.latitude;
+//       _longitude.value = value.longitude;
+//       //calling our weather api
+//       return FetchWeatherApi().processData(value.latitude, value.longitude).then((value) {
+//         weatherData.value = value;
+//          _isLoading.value = false;
+//       }
+//      );
+//     });
+//   }
   
-  RxInt getIndex() {
-    return _currentIndex;
-  }
-}
+//   RxInt getIndex() {
+//     return _currentIndex;
+//   }
+// }
